cad379db43e4d85b40bee045e92e8d38472bc9d4
Takeshi Kobayashi <tkoba@amazon.com>
2025-03-12 15:09:03 -0700

    Initial commit

---
+++ test/cross-account-qindex-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CrossAccountQindexDemo from '../lib/cross-account-qindex-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cross-account-qindex-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CrossAccountQindexDemo.CrossAccountQindexDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
