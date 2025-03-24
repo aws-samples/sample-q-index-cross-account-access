212f612fed393b0b587b235ba214c1b66b935fc2
Takeshi Kobayashi <tkoba@amazon.com>
2025-03-24 13:38:55 -0700

    init

--- cdk-stack/bin/cdk-stacks.ts
+++ cdk-stack/bin/cdk-stacks.ts
@@ -2,8 +2,11 @@
 
 import * as cdk from 'aws-cdk-lib';
 import { EnterpriseStack } from '../lib/enterprise-stack';
+import { AwsSolutionsChecks } from 'cdk-nag';
+import { Aspects } from 'aws-cdk-lib';
 
 const app = new cdk.App();
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }))
 
 
 new EnterpriseStack(app, 'EnterpriseStack', {
--- cdk-stack/lib/enterprise-stack.ts
+++ cdk-stack/lib/enterprise-stack.ts
@@ -3,11 +3,17 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as cr from "aws-cdk-lib/custom-resources";
 import * as iam from "aws-cdk-lib/aws-iam";
-
+import { NagSuppressions } from 'cdk-nag';
 
 export class EnterpriseStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    //Nag suppressions at stack level all apply to a Custom Resource that is deployed under the hood
+    NagSuppressions.addStackSuppressions(this, [{
+      id: 'AwsSolutions-IAM4',
+      reason: 'AWS Custom Resource requires Lambda basic execution role for CloudWatch logging'
+    }])
 
     // Parameter for Identity Center Instance ARN
     const identityCenterInstanceArn = new cdk.CfnParameter(this, 'IdentityCenterInstanceArn', {
@@ -59,71 +65,138 @@ export class EnterpriseStack extends cdk.Stack {
       value: qBusinessIndex.attrIndexId
     });
 
-    // Create IAM Role for Lambda
+    // Create the Lambda function role
     const ingestDummyDataRole = new iam.Role(this, 'ingestDummyDataRole', {
       assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
       inlinePolicies: {
-          ['qBusinessPolicy']: new iam.PolicyDocument({
+        ['qBusinessPolicy']: new iam.PolicyDocument({
           statements: [
-              new iam.PolicyStatement({
+            new iam.PolicyStatement({
               actions: ['qbusiness:BatchPutDocument'],
-              resources: ['*'],
-              }),
+              resources: [
+                `arn:${cdk.Stack.of(this).partition}:qbusiness:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:application/*`
+              ],
+            }),
           ],
-          }),
-      },
-      managedPolicies: [
-          iam.ManagedPolicy.fromAwsManagedPolicyName(
-            'service-role/AWSLambdaBasicExecutionRole',
-          ),
-      ],
-    });
-
-    // Create Lambda function to start Glue Crawler. The Glue Crawler is created in the stack above. 
-    const ingestDummyData = new lambda.Function(this, 'IngestDummyData', {
-        code: lambda.Code.fromAsset('lib/lambda/ingestDummyData', {
-          bundling: {
-            image: lambda.Runtime.PYTHON_3_11.bundlingImage,
-            command: [
-                'bash', '-c',
-                'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output && chmod -R 755 /asset-output',
-            ],
-          },
         }),
-        runtime: lambda.Runtime.PYTHON_3_11,
-        handler: 'index.handler',
-        memorySize: 128,
-        timeout: cdk.Duration.minutes(15),
-        role: ingestDummyDataRole,
-        environment: { 
-          ApplicationId: qBusinessApp.attrApplicationId,
-          IndexId: qBusinessIndex.attrIndexId
-        },
+        ['cloudWatchPolicy']: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: [
+                'logs:CreateLogGroup',
+                'logs:CreateLogStream',
+                'logs:PutLogEvents'
+              ],
+              resources: [
+                `arn:${cdk.Stack.of(this).partition}:logs:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:log-group:/aws/lambda/*`
+              ]
+            })
+          ]
+        })
+      }
     });
 
-    // Define the custom resource to invoke the Lambda function
+    // Create Lambda function
+    const ingestDummyData = new lambda.Function(this, 'IngestDummyData', {
+      code: lambda.Code.fromAsset('lib/lambda/ingestDummyData', {
+        bundling: {
+          image: lambda.Runtime.PYTHON_3_12.bundlingImage,
+          command: [
+            'bash', '-c',
+            'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output && chmod -R 755 /asset-output',
+          ],
+        },
+      }),
+      runtime: lambda.Runtime.PYTHON_3_12,
+      handler: 'index.handler',
+      memorySize: 128,
+      timeout: cdk.Duration.minutes(15),
+      role: ingestDummyDataRole,
+      environment: {
+        ApplicationId: qBusinessApp.attrApplicationId,
+        IndexId: qBusinessIndex.attrIndexId
+      },
+    });
+
+    // Create the custom resource role without Lambda function reference
+    const customResourceRole = new iam.Role(this, 'CustomResourceRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      inlinePolicies: {
+        ['cloudWatchPolicy']: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: [
+                'logs:CreateLogGroup',
+                'logs:CreateLogStream',
+                'logs:PutLogEvents'
+              ],
+              resources: [
+                `arn:${cdk.Stack.of(this).partition}:logs:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:log-group:/aws/lambda/*`
+              ]
+            })
+          ]
+        })
+      }
+    });
+
+    // Add Lambda invoke permission after Lambda creation
+    customResourceRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['lambda:InvokeFunction'],
+        resources: [ingestDummyData.functionArn]
+      })
+    );
+
+    // NAG suppressions
+    NagSuppressions.addResourceSuppressions(ingestDummyDataRole, [
+      {
+        id: 'AwsSolutions-IAM5',
+        reason: 'Wildcard is required for BatchPutDocument operations across all documents in the Q Business index',
+        appliesTo: [
+          `Resource::arn:${cdk.Stack.of(this).partition}:qbusiness:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:application/*`
+        ]
+      },
+      {
+        id: 'AwsSolutions-IAM5',
+        reason: 'Lambda function requires permissions to create and write to its CloudWatch log groups',
+        appliesTo: [
+          `Resource::arn:${cdk.Stack.of(this).partition}:logs:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:log-group:/aws/lambda/*`
+        ]
+      }
+    ]);
+
+    // NAG suppressions for the CustomResourceRole
+    NagSuppressions.addResourceSuppressions(customResourceRole, [
+      {
+        id: 'AwsSolutions-IAM5',
+        reason: 'Custom Resource Lambda function requires permissions to create and write to its CloudWatch log groups',
+        appliesTo: [
+          `Resource::arn:${cdk.Stack.of(this).partition}:logs:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:log-group:/aws/lambda/*`
+        ]
+      }
+    ]);
+
+    // Create custom resource with static physical ID
     const customResource = new cr.AwsCustomResource(this, 'IngestDummyDataCustomResource', {
       onCreate: {
         service: 'Lambda',
         action: 'invoke',
-        physicalResourceId: cr.PhysicalResourceId.of('IngestDummyData'),
+        physicalResourceId: cr.PhysicalResourceId.of('CustomResourceIngestDummyData-' + Math.random().toString(36).slice(2, 7)),
         parameters: {
           FunctionName: ingestDummyData.functionName,
           InvocationType: 'RequestResponse',
           Payload: '{}',
         },
-        region: this.region, 
+        region: this.region,
       },
-      policy: cr.AwsCustomResourcePolicy.fromStatements([
-          new iam.PolicyStatement({
-            actions: ['lambda:InvokeFunction'],
-            resources: [ingestDummyData.functionArn],
-            effect: iam.Effect.ALLOW,
-          }),
-      ]),
+      role: customResourceRole,
       installLatestAwsSdk: false,
     });
     customResource.node.addDependency(ingestDummyData);
+    customResource.node.addDependency(customResourceRole);
 
   }
 }
