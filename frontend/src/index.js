0a7e8c8b668241cab684c88fb6a4ce2622ae6c9a
Takeshi Kobayashi <tkoba@amazon.com>
2025-05-08 16:09:40 -0700

    adding single account - still inprogress

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import SingleAccountApp from './SingleAccountApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/single" element={<SingleAccountApp />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
